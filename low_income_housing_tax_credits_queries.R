08ca8ac802d80767283e5df65759f98d70cae638
Sebastian Langan <sebastian.langan@gmail.com>
2023-04-03 20:49:33 -0400

    EOD1 progress

--- low_income_housing_tax_credits_queries.R
+++ low_income_housing_tax_credits_queries.R
@@ -1,5 +1,13 @@
 #Load necessary packages for analysis: 
-
+library(bigrquery)
+library(DBI)
+library(dplyr)
 
 #Pull in the database table from Google BigQuery using BigQuery R API: 
 
+low_income_data_con <- 'deep-span-382614'
+sql <- "SELECT * FROM `low_income_housing_tax_credit_program.2017_lihtc_database_hud`"
+
+tb <- bq_project_query(low_income_data_con, sql)
+
+bq_table_download(tb, n_max = 10)
