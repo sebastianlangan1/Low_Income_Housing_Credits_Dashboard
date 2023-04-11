094935da8cb01b16de817f2462058416cf192d37
Sebastian Langan <sebastian.langan@gmail.com>
2023-04-10 23:45:58 -0400

    end of night progress

--- low_income_housing_tax_credits_queries.R
+++ low_income_housing_tax_credits_queries.R
@@ -77,8 +77,10 @@ q3_box_plot
 #It looks like Maryland and Florida are roughly tied for the highest average allocation amount. 
 
 #Question 4: Exploratory Analysis: Across all properties, what is the correlation between average per capita state income and total allocations per state? 
-#Does this relationship change after normalizing each states' total allocation by population? 
 #Given the aims of the LIHTC program, it would make sense for poorer states to receive more housing tax credit allocations. 
+#Does this relationship change after normalizing each housing project's allocations by state population? 
+#If state populations are a moderator or mediator of this relationship, then controlling for population this way might decrease the correlation 
+#between mean state income and LIHTC allocation, warranting further investigation. 
 
 #Pull in raw state income data from 2017: 
 incomes_bls_2017 <- read.xlsx("state_M2017_dl.xlsx") %>% filter(OCC_TITLE == 'All Occupations')
@@ -88,14 +90,23 @@ incomes_bls_2017 <- read.xlsx("state_M2017_dl.xlsx") %>% filter(OCC_TITLE == 'Al
 incomes_bls_2017 <- incomes_bls_2017 %>% select(STATE, A_MEAN)
 
 #Create column with state abbreviations in the BLS data: 
+incomes_bls_2017 <- incomes_bls_2017 %>% mutate(STATE_ABB = state.abb[match(STATE, state.name)])
+q3_dat <- q3_dat %>% rename(STATE_ABB = State)
 
+#Join BLS state income data onto the main LIHTC data frame for this question: 
+q3_dat <- q3_dat %>% left_join(incomes_bls_2017, by = c('STATE_ABB')) #Join by state abbreviation.
+q3_dat$A_MEAN <- as.numeric(q3_dat$A_MEAN)
 
-q3_dat <- q3_dat %>% left_join() #Join by state abbreviation.
+#Plot mean income per state (across all occupations) versus projects' allocation amounts. Also overlay the OLS linear regression trendline and its statistics. 
+stateincome_allocation_lm <- lm(A_MEAN ~ Allocation, data = q3_dat)
+summary(stateincome_allocation_lm)
+#There does appear to be a significant correlation between mean state income and total LIHTC allocations. 
+q4_lr_plot <- ggplot(q3_dat, aes(x = A_MEAN, y = Allocation)) + geom_point() + geom_smooth(method = 'lm', col = 'black') + ylim(0, 2500000) + xlab('2017 Mean State Incomes ($)') + 
+  ylab('2017 LIHTC Allocation ($)') + ggtitle('2017 Mean State Incomes vs. 2017 LIHTC Allocations per Housing Project')
 
-#Plot mean income per state (across all occupations) versus projects' allocation amounts. Also overlay the OLS linear regression trendline and its statistics.  
-q4_lr_plot <- ggplot(q4_dat, aes(x = State, y = Allocation)) + 
-  geom_boxplot() + ylim(0, 2500000)
+q4_lr_plot
 
+#Checking whether this relationship still holds when project allocations are normalized 
 
 
 #Question 5:
